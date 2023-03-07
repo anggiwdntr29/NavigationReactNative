c0915fdb57da49537d837ff76f6c7fad6e8a794c
anggiwdntr29 <widiantara2906@gmail.com>
2023-03-07 15:52:10 +0800

    menambah form login

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,20 +1,117 @@
-import { Button, View, Text, StyleSheet } from "react-native";
+import {
+  NativeBaseProvider,
+  Box,
+  Text,
+  Stack,
+  HStack,
+  VStack,
+  Pressable,
+  Image,
+  Center,
+  Button,
+  Divider,
+  Container,
+  Circle,
+  FormControl,
+  Input,
+  Link,
+} from "native-base";
+import React, { Component } from "react";
+import { Alert } from "react-native";
 
-function Login({ navigation }) {
-  return (
-    <View style={Styles.container}>
-      <Text>Ini Login</Text>
-      <Button title="Login" onPress={() => navigation.replace("ButtonNav")} />
-    </View>
-  );
+class Login extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      email: "",
+      password: "",
+    };
+  }
+
+  onLogin() {
+    const { email, password } = this.state;
+
+    console.log(`${email} + ${password}`);
+    this.props.navigation.replace("ButtonNav");
+  }
+
+  onRegistrasi() {
+    this.props.navigation.replace("Registrasi");
+  }
+
+  render() {
+    return (
+      <NativeBaseProvider>
+        <Center px="43px" pt="37">
+          <Box bg="primary.600" width="100%" height="200" safeAreaTop />
+          <Text
+            fontSize={"2xl"}
+            fontWeight={"semibold"}
+            mt={"42px"}
+            textAlign={"justify"}
+            width="100%"
+          >
+            Login to Continue
+          </Text>
+          <Text
+            width="100%"
+            fontSize={"md"}
+            fontWeight={"light"}
+            mt={"15px"}
+            textAlign={"justify"}
+          >
+            Log in to your account and continue your learning journey in Koding
+            Akademi.
+          </Text>
+
+          <FormControl>
+            <Stack space={5}>
+              <Stack>
+                <FormControl.Label>Username</FormControl.Label>
+                <Input
+                  variant={"outline"}
+                  p={3}
+                  value={this.state.email}
+                  onChangeText={(email) => this.setState({ email })}
+                  placeholder={"example@gmail.com"}
+                />
+              </Stack>
+              <Stack>
+                <FormControl.Label>Password</FormControl.Label>
+                <Input
+                  variant={"outline"}
+                  p={3}
+                  value={this.state.password}
+                  onChangeText={(password) => this.setState({ password })}
+                  placeholder={"********"}
+                  secureTextEntry={true}
+                />
+              </Stack>
+            </Stack>
+          </FormControl>
+          <Button
+            width="100%"
+            mt={6}
+            variant="solid"
+            size={"md"}
+            onPress={this.onLogin.bind(this)}
+          >
+            Login
+          </Button>
+          <Text>
+            Don't have an account?{" "}
+            <Link onPress={this.onRegistrasi.bind(this)}>
+              <Text fontWeight={"bold"}>Register here.</Text>
+            </Link>
+          </Text>
+          <Text mt={5} fontSize={"xs"} fontWeight={"thin"}>
+            Or Login With
+          </Text>
+          <Circle bg={"primary.600"} width={12} height={12} mt={4}></Circle>
+        </Center>
+      </NativeBaseProvider>
+    );
+  }
 }
-
-const Styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-  },
-});
-
 export default Login;
